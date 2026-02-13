1016d84920a9ab59037c6a05558d5d1dada30558
Akash Meshram <146848524+Akashmesh@users.noreply.github.com>
2026-02-13 09:24:47 +0000

    third commit

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -34,18 +34,18 @@ const App = () => {
         <div className='flex-1 flex-col'>
           <div className='flex flex-col'>
             <p className='text-5xl font-bold leading-15 tracking-wide text-gray-900'>Where Every Pet <br /> Gets The Royal <br /> Treatment</p>
-            <p className='tracking-wide leading-6 mt-7 text-gray-700'>Professional grooming, loving daycare, and comfortable <br /> boarding for your furry family members. Because they deserve br
+            <p className='tracking-wide leading-6 mt-7 text-gray-700'>Professional grooming, loving daycare, and comfortable <br /> boarding for your furry family members. Because they deserve <br />
              the best.</p>
           </div>
           <div className='mt-7 flex gap-4'>
             <button className='text-white bg-yellow-500 font-semibold px-5 py-2 rounded-full hover:bg-yellow-600 active:scale-95'>Book an Appointment</button>
             <button className='text-yellow-500 bg-white border border-yellow-500 font-semibold px-5 py-2 rounded-full hover:bg-yellow-500 hover:text-white transition duration-300 active:scale-95'>View Services</button>
           </div>
-          <div className=''>
-            <p className=''><span>10k+</span>Happy Pets</p>
-            <p className=''><span>8+</span>Years Experience</p>
-            <p className=''><span>4.9</span>Average Rating</p>
-            <p className=''><span>50+</span>Services</p>
+          <div className='flex justify-baseline gap-8 mt-10'>
+            <p className='flex flex-col text-gray-500 text-md'><span className='text-yellow-500 font-bold text-4xl'>10K+</span>Happy Pets</p>
+            <p className='flex flex-col text-gray-500 text-md'><span className='text-yellow-500 font-bold text-4xl'>8+</span>Years Experience</p>
+            <p className='flex flex-col text-gray-500 text-md'><span className='text-yellow-500 font-bold text-4xl'>4.9</span>Average Rating</p>
+            <p className='flex flex-col text-gray-500 text-md'><span className='text-yellow-500 font-bold text-4xl'>50+</span>Services</p>
           </div>
         </div>
         {/* right side */}
@@ -53,6 +53,12 @@ const App = () => {
 
         </div>
       </div>
+      {/* services section */}
+      <div className='mt-30'>
+        <h2 className='text-center font-bold text-5xl'>Our Services</h2>
+        <p className='text-center mt-2 text-gray-600 text-xl'>Everything your pet needs for a happy, healthy life</p>
+      </div>
+
     </div>
   )
 }
